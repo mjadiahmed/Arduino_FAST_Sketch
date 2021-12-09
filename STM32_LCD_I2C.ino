c56c44f440f9e02a72ffa2dacf380479d34f7da3
Ahmed MJADI <92577300+mjadiahmed@users.noreply.github.com>
2021-12-09 16:22:01 +0100

    Update STM32_LCD_I2C.ino

--- STM32_LCD_I2C.ino
+++ STM32_LCD_I2C.ino
@@ -23,7 +23,7 @@
 // This sketch tests the standard 7-bit addresses
 // Devices with higher bit address might not be seen properly.
 //
-
+// Modified by Ahmed MJADI
 
 
 /* Example pinmap for Bluepill I2Cs (by Testato)
